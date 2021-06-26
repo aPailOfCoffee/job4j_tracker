a2776cf4d3c7d1bbd24836027d79c961dbeb4cdb
aPailOfCoffee <bshestakoff@gmail.com>
2021-06-26 11:01:48 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,13 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String currentDateTime = item.getCreated().format(formatter);
-        System.out.println(currentDateTime);
+        System.out.println(item);
     }
 }
