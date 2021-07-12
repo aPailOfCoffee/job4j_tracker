e0e9c0cd9dd79ea76d8066168c20ec8e61780446
aPailOfCoffee <bshestakoff@gmail.com>
2021-07-12 21:42:31 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("ivanov1990@gmail.com", "Ivan Ivanov");
+        map.put("petrov1991@gmail.com", "Petr Petrov");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
