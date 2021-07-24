0eaeb7cb67c8f213e8d6d4059167b2f11cd753b7
aPailOfCoffee <bshestakoff@gmail.com>
2021-07-24 21:21:41 +0300

    1.2. Функциональный интерфейс [#247497]

---
+++ src/main/java/function/FunctionalInterfaces.java
@@ -0,0 +1,34 @@
+package function;
+
+import java.util.*;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> bicon = (key, value) -> map.put(key, value);
+        bicon.accept(1, "one");
+        bicon.accept(2, "two");
+        bicon.accept(3, "three");
+        bicon.accept(4, "four");
+        bicon.accept(5, "five");
+        bicon.accept(6, "six");
+        bicon.accept(7, "seven");
+
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 && s.length() == 4;
+        for (Integer i : map.keySet()) {
+            if (biPred.test(i, map.get(i))) {
+                System.out.println("key: " + i + " value: " + map.get(i));
+            }
+        }
+
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        List<String> strings = sup.get();
+
+        Consumer<String> con = (s) -> System.out.println(s);
+        Function<String, String> func = (s) -> s.toUpperCase();
+        for (String s : strings) {
+            con.accept(func.apply(s));
+        }
+    }
+}
