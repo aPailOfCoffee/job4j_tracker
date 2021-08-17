3056409f11d6b8fb4965afa3174283d088b26d9e
aPailOfCoffee <bshestakoff@gmail.com>
2021-08-17 12:07:36 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Subject.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+
+public class Subject {
+    private String name;
+    private int score;
+
+    public Subject(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+}
